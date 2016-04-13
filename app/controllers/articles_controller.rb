49d5d47e957410424d3150ad5fa77729aa45b5e3
Rails Course <arijeetbhattacharya@outlook.com>
2016-04-13 01:29:06 +0000

    Template, Model and Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   end
   
   def create
+#   render plain: params[:article].inspect  
     @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
